be7ac4944ab7387e3b91a41e16e6671758ba051e
Jon Coppeard <jcoppeard@mozilla.com>
2026-02-06 11:39:10 +0000

    Bug 1693542 - Part 2: Add GC parameter and shell options to control concurrent marking r=sfink,dom-worker-reviewers,edenchuang
    Differential Revision: https://phabricator.services.mozilla.com/D279706

--- js/src/gc/GCRuntime.h
+++ js/src/gc/GCRuntime.h
@@ -462,6 +462,14 @@ class GCRuntime {
     return state() != State::NotActive && !isVerifyPreBarriersEnabled();
   }
 
+  bool isConcurrentMarkingEnabled() const {
+#ifndef JS_GC_CONCURRENT_MARKING
+    return false;
+#else
+    return concurrentMarkingEnabled;
+#endif
+  }
+
   bool hasForegroundWork() const;
 
   bool isShrinkingGC() const { return gcOptions() == JS::GCOptions::Shrink; }
@@ -1354,6 +1362,15 @@ class GCRuntime {
    * pref: javascript.options.mem.gc_parallel_marking
    */
   MainThreadData<bool> parallelMarkingEnabled;
+
+#ifdef JS_GC_CONCURRENT_MARKING
+  /*
+   * Whether concurrent marking is enabled globally.
+   *
+   * JSGC_CONCURRENT_MARKING_ENABLED
+   */
+  MainThreadData<bool> concurrentMarkingEnabled;
+#endif
 
   MainThreadData<bool> rootsRemoved;
 
